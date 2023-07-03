65505d0651e5556d883715e4d0893fa811f3745a
EmanuelLeocadio <emanuelleocadioramalho@gmail.com>
2023-07-03 09:39:36 -0300

    first commit

---
+++ src/main/java/br/com/tech4me/tech4jogadores/Tech4jogadoresApplication.java
@@ -0,0 +1,13 @@
+package br.com.tech4me.tech4jogadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tech4jogadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tech4jogadoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tech4me/tech4jogadores/Tech4jogadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tech4me.tech4jogadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tech4jogadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
